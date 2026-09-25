46e8a6ee1fbdc1b71cdead298f138f44adfec1ce
Ashrafshehab <cairo.cairo@yahoo.com>
2026-09-26 01:22:16 +0300

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -6,6 +6,7 @@ const path = require("path");
 const { execSync } = require("child_process");
 
 module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("src/admin");
 eleventyConfig.addPassthroughCopy("src/admin");
   // ─── Plugins ────────────────────────────────────────────────────────────────
   eleventyConfig.addPlugin(pluginRss);
